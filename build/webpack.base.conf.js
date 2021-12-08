896c1f1bd129c38c762dd5825663ed238e39cd7c
Jhonah <buryakone@gmail.com>
2021-12-08 22:49:06 +0700

    commit message

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,126 @@
+/* webpack 4 */
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/* Main const */
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
+}
+
+/* Pages const for HtlmWebpackPlugin */
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+/* const PAGES_CSS = `${PATHS.assets}fonts/Quicksand/` */
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+    // BASE config
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src,
+        /* main: './src/index.js' */
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].[hash].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }, {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'
+                }
+            }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]'
+                }
+            }, {            
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [
+                                    [ 'autoprefixer', { /* Options */ }, ],
+                                ],
+                            },
+                        },
+                    }, {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [
+                                    [ 'autoprefixer', { /* Options */ }, ],
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].[hash].css`,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+                { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+                //{ from: `${PATHS.src}/static`, to: '' },
+            ],
+        }),
+        ...PAGES.map(page =>
+            new HtmlWebpackPlugin({
+            hash: true,
+            template: `${PAGES_DIR}/${page}`,   
+            filename: `./${page.replace(/\.pug/, '.html')}`
+            })),
+    ],
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss';
+
+//console.log("hello, world");
+//console.log("hello, world 2");
