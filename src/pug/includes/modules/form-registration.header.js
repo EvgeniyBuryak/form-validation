7fb274f5e0a1e7e1c7f0d364502cbbed34b3d7d8
Jhonah <buryakone@gmail.com>
2021-12-14 22:00:23 +0700

    предварительная настройка времени

--- src/pug/includes/modules/form-registration.header.js
+++ src/pug/includes/modules/form-registration.header.js
@@ -3,6 +3,7 @@ let rePhone = /\(?\d{3}\)?([-\/\.])\d{3}\1\d{4}/;
 let reName = new RegExp("^[А-ЯЁ][а-яё\\s]+$");
 let reEmail = new RegExp("^[A-Za-z0-9]+[A-Za-z0-9-_.]*[A-Za-z0-9]+@[A-Za-z]+\\.([A-Za-z]{2,4})+$");
 let rePassword = new RegExp("(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])(?=.*[\\W])[\\w\\W]{8,}"); // позитивный просмотр вперед
+let reDate = new RegExp("[0-9]{4}-[0-9]{2}-[0-9]{2}");
 // let rePassword = new RegExp("[A-Za-z]+(?=.*\\d{1,})?"); // позитивный просмотр вперед
 // let rePassword = new RegExp("^(([A-Za-z0-9]+\\d+))+$");
 // let reEmail = /^[A-Za-z0-9]+@[A-Za-z]+\.com$/;
@@ -16,36 +17,38 @@ let rePassword = new RegExp("(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])(?=.*[\\W])[\\w\\W
 //         window.alert("Thanks, your phone number is " + OK[0]);
 // }
 
+// массив имен инпутов
 const ARR_TYPE = { 
     text: 'Кириллицей',
     email: 'Email',
     password: 'пароль',
+    date: 'дата',
 };
 
+// массив регулярных выражений
 const ARR_REGEXP = { 
     text: reName,
     email: reEmail,
     password: rePassword,
+    date: reDate,
 };
 
+const passConfirm = (value) => {
+    if (RegExp.input == value)
+        console.log(`Пароль: ${RegExp.input} совпадает`);
+}
+
 const checkValue = (value, titleType, re) => {
-    //for (let [type, re] of Object.entries(ARR_REGEXP)) {
-        
-        //if (type != inputType) continue;
+    //if (type != inputType) continue;
 
-        const prevRegExp = RegExp.input;
+    if (ARR_TYPE['password'] == titleType) passConfirm(value);
 
-        if (ARR_TYPE['password'] == titleType) 
-            if (RegExp.input == value)
-                console.log(`Картошка: prev: ${prevRegExp} new: ${RegExp.input}`);
+    const OK = re.exec(value);
 
-        const OK = re.exec(value);
-
-        if(!OK)
-            alert(RegExp.input + ` - Неверный ${titleType}`);
-        else 
-            alert(`Thanks, your ${titleType} is ${OK[0]}`);
-    //}
+    if(!OK)
+        alert(RegExp.input + ` - Неверный ${titleType}`);
+    else 
+        alert(`Thanks, your ${titleType} is ${OK[0]}`);
 }
 
 function testInfo () {
@@ -60,8 +63,8 @@ function testInfo () {
         // есть подозрения что будет работать и без switch
         switch(type) {
             // case "text": 
-            //     console.log(type);
-            //     // console.log(input.value);
+            // //     console.log(type);
+            // //     // console.log(input.value);
             //     checkValue(input.value, ARR_TYPE.text, re);
             //     break;
             // case "email": 
@@ -69,9 +72,63 @@ function testInfo () {
             //     // console.log(input.value);
             //     checkValue(input.value, ARR_TYPE.email, re);
             //     break;
-            case "password": 
-                //console.log(type);
-                // console.log(input.value);
+            // case "password": 
+            //     //console.log(type);
+            //     // console.log(input.value);
+            //     checkValue(input.value, title, re);
+            //     break;
+            case "date":
+                
+                const isAdult = (value) => {
+                    let sevenHours = 7 * 3600 * 1000;
+                    let sixHours = 6 * 3600 * 1000;
+                    let oneDay = 24 * 3600 * 1000;
+                    // console.log(oneDay);
+
+                    // Текущая дата по гринвичу
+                    const currentDateTimeStamp = Date.now(); // create date in the milliseconds UTC+0
+                    // Пользовательская дата по НСК
+                    const userDateTimeStamp = Date.parse(value); // from string to milliseconds UTC+07
+                    
+                    // Дата в читаемом виде 2012-12-12 + time
+                    let currentDate = new Date(currentDateTimeStamp);
+                    let userDate = new Date(userDateTimeStamp);// - sixHours);
+                    
+                    // const eighteenYearsAgo = currentDate.getFullYear() - 18;
+                    // const user = userDate.getFullYear();
+
+                    let eighteenYearsAgo = new Date(
+                        currentDate.getFullYear() - 18, 
+                        currentDate.getMonth(), 
+                        currentDate.getDate(),
+                    );
+                    
+                    let userYear = new Date(
+                        userDate.getFullYear(),
+                        userDate.getMonth(), 
+                        userDate.getDate(),
+                        0,                
+                        //'2012-01-26T13:51:50.417-07:00'        
+                    );
+
+                    // console.log(Date.parse(newDate.toString()));
+                    console.log(`currentDate: ${currentDate}`);
+                    console.log(`old18Plus: ${eighteenYearsAgo}`);
+                    console.log(`userDate: ${userDate}`);
+                    console.log(`userYear: ${userYear}`);
+    
+                    if (userDateTimeStamp < currentDateTimeStamp) {
+                        console.log('Есть 18 лет!');
+                    }
+
+                    const res = currentDate - userDate;// * (24 * 3600 * 1000);
+
+                    console.log(`Из текущей даты вычитаем дату пользователя: ${res}`); //new Date(oneDay)
+                }
+
+                // console.log(input.value.split("-")[0]);
+                isAdult(input.value);
+
                 checkValue(input.value, title, re);
                 break;
             case "reset":                
@@ -81,7 +138,7 @@ function testInfo () {
                 break;
                 
         }
-        console.log(input.value);
+        // console.log(input.value);
         // if (input.getAttribute('type') == "text") {
         //     // console.log(input.value);
         //     checkValue(input.value);
